02654fcd7709d7d74d7ca65543b9f83e30094b61
Kalyan Vedala <svedala23@gmail.com>
2023-07-23 09:36:18 -0700

    Install dotenv, sqlite3 for dev

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 const port = 3000
 
-app.post('/createInstance', (req, res) => {
-  res.send('createInstance request received')
+app.use(express.json())
+
+app.post('/instances', async (req, res) => {
+
+  // create a row in instances table
+
+  // TODO: append task to a queue
+
+  res.send({ msg: 'createInstance request received' })
 })
 
 app.listen(port, () => {
