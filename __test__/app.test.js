24820e25a923b1576b0fd40f8619e40046d2fcae
Kalyan Vedala <svedala23@gmail.com>
2024-01-07 17:40:36 -0800

    Update test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,12 +1,16 @@
-import server from '../app.js';
-const supertest = require('supertest');
-const requestWithSuperTest = supertest(server);
+import request from 'supertest';
+import express from 'express';
+import router from '../routes.js';
+
+const app = express();
+app.use('/', router);
 
 describe('Instance endpoints', () => {
 
   it('GET /instances should get all instances', async () => {
-    const res = await requestWithSuperTest.get('/instances');
+    const res = await request(app).get('/instances');
     expect(res.status).toEqual(200);
     expect(res.body).toHaveProperty('instances');
   });
+
 });
