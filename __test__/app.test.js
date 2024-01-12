c2c4753c2334b08e42ec1415ea4f13012c8a1336
Kalyan Vedala <svedala23@gmail.com>
2024-01-12 15:41:38 -0800

    Try mocking knex

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,10 +1,23 @@
+jest.mock('knex', () => {
+  select: jest.fn().mockReturnThis("111")
+});
 import request from 'supertest';
 import app from '../app';
+import {jest} from '@jest/globals';
+import Knex from 'knex';
+import router from '../routes';
 
+// const mockKnex = jest.fn();
+
+// mockKnex.mockImplementation(() => () => ({
+//   select: jest.fn().mockReturnThis("111")
+// }))
+// jest.mock('knex', () => jest.fn(console.log("hello")));
 describe('Instance endpoints', () => {
 
   it('GET /instances should get all instances', async () => {
     const res = await request(app).get('/instances');
+    console.log(res.body);
     expect(res.status).toEqual(200);
     expect(res.body).toHaveLength(43);
   });
