9b481df96be318a6e343652b16829789217b6aca
Kalyan Vedala <svedala23@gmail.com>
2024-01-13 07:29:48 -0800

    Create handlers

---
+++ handlers.js
@@ -0,0 +1,54 @@
+import sendToCreateInstanceQueue from './rabbit.js';
+
+const getHealth = async (req, res) => {
+  res.status(200)
+  res.send("Backend is alive")
+};
+
+const postInstances = async (req, res) => {
+  const instanceName = req.body.instanceName
+  const instance = {
+    name: instanceName
+  }
+
+  try {
+    const instanceIdArr = await req.dataService.postInstances(instance);
+    const instanceId = instanceIdArr[0].id;
+
+    // append task to queue
+    console.log("instanceId returned=", instanceId);
+    sendToCreateInstanceQueue(instanceId);
+
+    res.send({ msg: 'createInstance request received' })
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ err: 'Error in postInstances handler'});
+  }
+};
+
+const getInstances = async (req, res) => {
+
+  try {
+    const rows = await req.dataService.getInstances();
+    res.send(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ err: 'Error in getInstance handler'});
+  }
+};
+
+const getOneInstance = async (req, res) => {
+  try {
+    const rows = await req.dataService.getOneInstance(req.params.inst_id);
+    if (rows.length === 0) {
+      res.status(404).send({ msg: "Not found" })
+    }
+
+    res.send(rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ err: 'Error in getOneInstance handler'});
+  }
+};
+
+export { getHealth, postInstances, getInstances, getOneInstance };
