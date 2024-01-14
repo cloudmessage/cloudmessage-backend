44d12fbdb601e1ffb920f3df9bae3e9563627efc
Kalyan Vedala <svedala23@gmail.com>
2024-01-14 05:23:54 -0800

    Install sinon, update test

--- test/test.js
+++ test/test.js
@@ -2,8 +2,21 @@
 import request from 'supertest';
 import app from '../app.js';
 import { expect } from 'chai';
+import Knex from 'knex';
+// import { createSandbox } from 'sinon';
+import sinon from 'sinon';
+
+// const sandbox = createSandbox();
+// const mockKnex = () => {};
 
 describe('Instance endpoints', () => {
+
+  const knex = Knex();
+
+  beforeEach(() => {
+    // sandbox.stub(knex, 'default').callsFake(mockKnex);
+    sinon.stub(knex, 'select').returns('111');
+  });
 
   it('GET /instances should get all instances', async () => {
     const res = await request(app).get('/instances');
