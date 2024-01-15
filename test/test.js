7380027f3d156cb0d0f8272269d32713b373fdae
Kalyan Vedala <svedala23@gmail.com>
2024-01-15 08:22:45 -0800

    Cleanup test

--- test/test.js
+++ test/test.js
@@ -2,23 +2,10 @@
 import request from 'supertest';
 import app from '../app.js';
 import { expect } from 'chai';
-import Knex from 'knex';
-// import { createSandbox } from 'sinon';
-import sinon from 'sinon';
 
-// const sandbox = createSandbox();
-// const mockKnex = () => {};
+describe('Instance routes', () => {
 
-describe('Instance endpoints', () => {
-
-  const knex = Knex();
-
-  beforeEach(() => {
-    // sandbox.stub(knex, 'default').callsFake(mockKnex);
-    sinon.stub(knex, 'select').returns('111');
-  });
-
-  it('GET /instances should get all instances', async () => {
+  it('router /getInstances should return all instances', async () => {
     const res = await request(app).get('/instances');
     console.log(res.body);
     expect(res.status).to.equal(200);
