745cf83a067ae8d50ccbbf4867c53b5063422091
Kalyan Vedala <svedala23@gmail.com>
2024-01-15 11:57:30 -0800

    Fix test to make it not hang

--- test/test.js
+++ test/test.js
@@ -1,15 +1,49 @@
 
 import request from 'supertest';
 import app from '../app.js';
+import express from 'express';
 import { expect } from 'chai';
+import Knex from 'knex';
+import router from '../routes.js';
+import { postInstances, getInstances, getOneInstance } from '../data.js';
+import sinon from 'sinon';
 
 describe('Instance routes', () => {
 
+  const expectedValue = [
+    {id: 10, name: "instance10"},
+    {id: 20, name: "instance20"}
+  ];
+  const selectStub = sinon.stub().returnsThis();
+  const orderByStub = sinon.stub().resolves(expectedValue);
+
+  const knexStub = sinon.stub().callsFake(() => {
+    return {
+      select: selectStub,
+      orderBy: orderByStub
+    }
+  })
+  const app = express();
+  const dataService = () => {
+    return Object.freeze({
+      postInstances,
+      getInstances,
+      getOneInstance
+    });
+  };
+
+  const exposeDataService = async(req, res, next) => {
+    req.dataService = dataService();
+    req.knex = knexStub;
+    next();
+  }
+
+  app.use('/', exposeDataService, router);
+
   it('router /getInstances should return all instances', async () => {
     const res = await request(app).get('/instances');
-    console.log(res.body);
     expect(res.status).to.equal(200);
-    expect(res.body).to.have.length(43);
+    expect(res.body).to.have.length(2);
   });
 
 });
