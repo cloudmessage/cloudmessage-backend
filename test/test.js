0f35e5e111b4e7f3b0cb3d4cdecee4ddf27311cf
Kalyan Vedala <svedala23@gmail.com>
2024-01-16 18:55:34 -0800

    Add test for getting one instance

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ import router from '../routes.js';
 import { postInstances, getInstances, getOneInstance } from '../data.js';
 import sinon from 'sinon';
 
-describe('Instance routes', () => {
+describe('getInstances route', () => {
 
   const expectedValue = [
     {id: 10, name: "instance10"},
@@ -45,6 +45,48 @@ describe('Instance routes', () => {
     expect(res.status).to.equal(200);
     expect(res.body).to.have.length(2);
     expect(res.body[0]).to.deep.equal({id: 10, name: "instance10"})
+  });
+
+});
+
+describe('getInstances/<id> route', () => {
+
+  const returnValue = [
+    {id: 10, name: "instance10"},
+    {id: 20, name: "instance20"}
+  ];
+  const expectedValue = {id: 10, name: "instance10"};
+  const selectStub = sinon.stub().returnsThis();
+  const whereStub = sinon.stub().resolves(returnValue);
+
+  const knexStub = sinon.stub().callsFake(() => {
+    return {
+      select: selectStub,
+      where: whereStub
+    }
+  })
+  const app = express();
+  const dataService = () => {
+    return Object.freeze({
+      postInstances,
+      getInstances,
+      getOneInstance
+    });
+  };
+
+  const exposeDataService = async(req, res, next) => {
+    req.dataService = dataService();
+    req.knex = knexStub;
+    next();
+  }
+
+  app.use('/', exposeDataService, router);
+
+  it('router /getInstances/10 should return correct instance', async () => {
+    app.use('/', exposeDataService, router);
+    const res = await request(app).get('/instances/10');
+    expect(res.status).to.equal(200);
+    expect(res.body).to.deep.equal({id: 10, name: "instance10"})
   });
 
 });
