58b7ba3e61e3dc55f0899f34772a07ad0b34e6d2
Kalyan Vedala <svedala23@gmail.com>
2024-01-18 10:02:08 -0800

    getHealth test, do not use dummyMiddleware; remove unused imports

--- test/getHealth.test.js
+++ test/getHealth.test.js
@@ -1,22 +1,14 @@
 
 import request from 'supertest';
-import app from '../app.js';
 import express from 'express';
 import { expect } from 'chai';
-import Knex from 'knex';
 import router from '../routes.js';
-import { postInstances, getInstances, getOneInstance } from '../data.js';
-import sinon from 'sinon';
 
 describe('getHealth route', () => {
 
   const app = express();
 
-  const dummyMiddleware = (req, res, next) => {
-    next();
-  }
-
-  app.use('/', dummyMiddleware, router);
+  app.use('/', router);
 
   it('router /getHealth should return expected response', async () => {
     const res = await request(app).get('/health');
