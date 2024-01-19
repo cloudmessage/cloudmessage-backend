1a149e25fc18ba0055e2d809a52c76f5193b04b5
Kalyan Vedala <svedala23@gmail.com>
2024-01-19 05:11:45 -0800

    Update authorize function

--- authorization.js
+++ authorization.js
@@ -1,5 +1,4 @@
-import { expressjwt as jwt} from 'express-jwt';
-import jwksRsa from 'jwks-rsa';
+import { auth } from 'express-oauth2-jwt-bearer';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,28 +6,10 @@ dotenv.config();
 const AUTH0_AUDIENCE = process.env.AUTH0_AUDIENCE;
 const AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL;
 
-const authorize = (req, res, next) => {
-  jwt({
-    secret: jwksRsa.expressJwtSecret({
-      jwksUri: `${AUTH0_ISSUER_BASE_URL}/.well-known/jwks.json`,
-    }),
-    audience: AUTH0_AUDIENCE,
-    issuerBaseURL: AUTH0_ISSUER_BASE_URL,
-    algorithms: ['RS256'],
-    getToken: function fromHeaderOrQuerystring(req) {
-      if (
-        req.headers.authorization &&
-        req.headers.authorization.split(" ")[0] === "Bearer"
-      ) {
-        return req.headers.authorization.split(" ")[1];
-      } else if (req.query && req.query.token) {
-        return req.query.token;
-      }
-      return null;
-    },
-  });
-
-  next();
-}
+const authorize = auth({
+  audience: AUTH0_AUDIENCE,
+  issuerBaseURL: AUTH0_ISSUER_BASE_URL,
+  algorithms: ['RS256'],
+});
 
 export default authorize;
