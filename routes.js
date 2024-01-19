5aa4188244bd77909cea7e9c7960f31cd70700ce
Kalyan Vedala <svedala23@gmail.com>
2024-01-19 05:12:08 -0800

    Use authorize for protected routes

--- routes.js
+++ routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getHealth, postInstances, getInstances, getOneInstance } from './handlers.js';
+import authorize from './authorization.js';
 
 const router = Router();
 
 router.get('/health', getHealth);
-router.post('/instances', postInstances);
-router.get('/instances', getInstances);
-router.get('/instances/:inst_id', getOneInstance);
+router.post('/instances', authorize, postInstances);
+router.get('/instances', authorize, getInstances);
+router.get('/instances/:inst_id', authorize, getOneInstance);
 
 export default router;
