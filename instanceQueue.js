06582848b5dddf816448b9099848831770562036
Kalyan Vedala <svedala23@gmail.com>
2024-01-26 15:51:38 -0800

    Modify to use async/promises api

--- instanceQueue.js
+++ instanceQueue.js
@@ -1,30 +1,16 @@
 import * as amqplib from 'amqplib';
 
 const sendToCreateInstanceQueue = async (instanceId) => {
-  amqplib.connect(process.env.INSTANCE_MQ_URL, async (error0, connection) => {
-    if (error0) {
-      throw error0
-    }
-  
-    connection.createChannel(function(error1, channel) {
-      if (error1) {
-        throw error1
-      }
-  
-      const queue = 'create_inst_queue'
-      var msg = String(instanceId)
-  
-      channel.assertQueue(queue, {
-        durable: true
-      })
-      channel.sendToQueue(queue, Buffer.from(msg), {
-        persistent: true
-      })
-    })
-    setTimeout(function() {
-      connection.close()
-    }, 500)
-  })
+  const queue = 'create_inst_queue';
+  const conn = await amqplib.connect(process.env.INSTANCE_MQ_URL);
+  const channel = await conn.createChannel();
+  await channel.assertQueue(queue);
+
+  var msg = String(instanceId)
+
+  channel.sendToQueue(queue, Buffer.from(msg), {
+    persistent: true
+  });
 }
 
 export default { sendToCreateInstanceQueue };
